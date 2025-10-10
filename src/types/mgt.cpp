c633f936036735eef61d515b20b3177576e2374a
Simon Owen <simon@simonowen.com>
2025-10-10 20:39:31 +0100

    Add fallback method for DISCiPLE IMG detection
    We previously relied on finding the sector chain for the first file sector,
    but that fails if the file is too short for a second sector.
    
    We now also check for a non-null header in the directory entry, and whether it
    matches the first 9 bytes of the file. That will be present for common CODE and
    BASIC file types.

--- src/types/mgt.cpp
+++ src/types/mgt.cpp
@@ -21,12 +21,22 @@ bool ReadMGT(MemFile& file, std::shared_ptr<Disk>& disk)
 
     // Check the expected chain offset in MGT images (alternating sides).
     std::array<uint8_t, 2> buf;
-    auto offset = (di.dir_tracks * MGT_SIDES * MGT_TRACK_SIZE) + SECTOR_SIZE - 2;
-    bool mgt = file.seek(offset) && file.read(buf) && buf[0] == di.dir_tracks && buf[1] == 2;
+    auto chain_offset = (di.dir_tracks * MGT_SIDES * MGT_TRACK_SIZE) + SECTOR_SIZE - 2;
+    bool mgt = file.seek(chain_offset) && file.read(buf) && buf[0] == di.dir_tracks && buf[1] == 2;
 
-    // Check the expected chain offset in IMG images (side 0 then side 1).
-    offset = (di.dir_tracks * MGT_TRACK_SIZE) + SECTOR_SIZE - 2;
-    bool img = file.seek(offset) && file.read(buf) && buf[0] == di.dir_tracks && buf[1] == 2;
+    // Check the expected chain offset in IMG images (tracks on same side).
+    chain_offset = (di.dir_tracks * MGT_TRACK_SIZE) + SECTOR_SIZE - 2;
+    bool img = file.seek(chain_offset) && file.read(buf) && buf[0] == di.dir_tracks && buf[1] == 2;
+
+    // Also check the file header against the first directory entry.
+    std::array<uint8_t, 9> dir_header{};
+    std::array<uint8_t, 9> file_header{};
+    auto dir_offset = 0xd3;
+    auto file_offset = di.dir_tracks * MGT_TRACK_SIZE;
+    img |= file.seek(dir_offset) && file.read(dir_header) &&
+        !std::equal(dir_header.begin(), dir_header.end(), file_header.begin()) && // zero check
+        file.seek(file_offset) && file.read(file_header) &&
+        std::equal(dir_header.begin(), dir_header.end(), file_header.begin());
 
     // If neither signature matched this probably isn't an MGT file.
     // However, accept it if it has a .mgt file extension.
